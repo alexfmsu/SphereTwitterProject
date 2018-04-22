8f3dba48f56b6d5841891627897dfb15ed3124ac
alexfmsu <alexfmsu@mail.ru>
2018-04-22 15:15:35 +0300

    config refresh

---
+++ config.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = consumer_key = 'hVdHWiKhV4ySqPaMG2jij8iCG'
+CONSUMER_SECRET = consumer_secret = 'SUqbcKA1oz33ziOfNghoQgx2G2FCoBm5bOtBIlSNnVbBynbYI3'
+ACCESS_TOKEN = access_token = access_key = '3064584671-HEsNDU3GcKlLBdL3dxq7t5hAU9RDkZ5MeIRvD78'
+ACCESS_TOKEN_SECRET = access_secret = 'o7weRvnH22L7ct3NTVTRK8aMKEGyARZYFjdOQxSLWfDiE'
+
+# MISSING_IMAGE = 'http://epaper2.mid-day.com/images/no_image_thumb.gif'
